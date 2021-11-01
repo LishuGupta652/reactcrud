5a8ac4bccb9fdfa4abfa02c56c1b5c942f1d25b7
LishuGupta652 <lishugupta652@gmail.com>
2021-11-01 04:31:36 +0000

    test

---
+++ crud2/src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <form>
+            <input type="text" placeholder="Enter a Todo..." className="input-form"/>
+            <button>Submit</button>
+        </form>
+
+    )
+}
+
+export default Form
