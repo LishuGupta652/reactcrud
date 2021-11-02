d45487ba13cdb276cc691b2fa7807fd577846044
LishuGupta652 <lishugupta652@gmail.com>
2021-11-02 15:39:16 +0000

    dotenv configured

--- crud5/index.js
+++ crud5/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const dotenv = require('dotenv');
 
+dotenv.config();
 
 // import routes 
 const authRoutes = require("./routes/auth");
